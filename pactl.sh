cbc1e086aeedb36b03def923f5eb1f7675c41e0b
onlyspxctre <onlyspxctre@proton.me>
2026-02-10 21:44:49 -0500

    pactl.sh: removed `output-ultimate`, use `loopback` module
    This fixes mix bussing issues with Equicord

--- pactl.sh
+++ pactl.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-pactl load-module module-null-sink media.class=Audio/Sink sink_name=output-ultimate
-pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=input-ultimate
-pactl load-module module-null-sink media.class=Audio/Sink sink_name=loopback
+pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=input-ultimate sink_properties=device.description="input-ultimate"
+pactl load-module module-loopback media.class=Audio/Sink sink_name=loopback sink_properties=device.description="loopback"
 
-pactl set-default-sink output-ultimate
 pactl set-default-source input-ultimate
 
-pactl set-sink-volume output-ultimate 100%
 pactl set-source-volume input-ultimate 100%
 
 carla ~/carla.carxp -n
 
 pactl unload-module module-null-sink
+pactl unload-module module-loopback
