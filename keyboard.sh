87580aaee74255afc79832bab46f8bd2cf08772f
onlyspxctre <onlyspxctre@proton.me>
2026-04-19 17:27:11 -0400

    hyprland: `keyboard.sh` dont reload config
    Prevent non-persistent display scaling to be reset upon activating or
    disabling the on-board keyboard.

--- keyboard.sh
+++ keyboard.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ $1 == 'on' ]; then
-    hyprctl -r -- keyword device[at-translated-set-2-keyboard]:enabled true
+    hyprctl keyword device[at-translated-set-2-keyboard]:enabled true
 elif [ $1 == 'off' ]; then
     sleep 0.05 # sleep to avoid the input not being flushed in time
-    hyprctl -r -- keyword device[at-translated-set-2-keyboard]:enabled false
+    hyprctl keyword device[at-translated-set-2-keyboard]:enabled false
 fi
