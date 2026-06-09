e989481ca19e115d1194c129328f68cd6bfd3023
onlyspxctre <onlyspxctre@proton.me>
2026-06-08 23:47:18 -0400

    firefox: added default config
    Default config (using Arkenfox's template) for Firefox.

---
+++ .config/mozilla/firefox/profilespxctre/user-overrides.js
@@ -0,0 +1,21 @@
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
+
+user_pref("browser.newtabpage.activity-stream.telemetry.privatePing.inferredInterests.enabled", false);
+
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("identity.fxaccounts.toolbar.enabled", false);
+user_pref("browser.ml.enabled", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.preferences.aiControls", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.vpn_promo.enabled", false);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
+user_pref("dom.forms.autocomplete.formautofill", false);
+
+user_pref("signon.autofillForms", false);
+user_pref("signon.rememberSignons", false);
+user_pref("signon.formlessCapture.enabled", false);
+user_pref("signon.management.page.breach-alerts.enabled", false);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
